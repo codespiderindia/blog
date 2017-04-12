ecb9ce17387fcf9e622b0a84a7dc53bb6f706fc2
codespiderindia <manojgorasya@gmail.com>
2017-04-12 13:36:55 +0530

    Pagination using will_paginate Gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,12 @@ class PostsController < ApplicationController
 	before_action :set_post,only: [:show,:edit,:update,:destroy]
 
 	def index
-		@posts = Post.all
+		#@posts = Post.all
+
+
+# or, use an explicit "per page" limit:
+@posts=Post.paginate(:page => params[:page], :per_page => 2)
+
 	end
 
 	def new
